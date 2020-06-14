a097861e852c0a6b54367bdc2df8d1eae3831b78
wwwjf <wwwjf2015@outlook.com>
2020-06-14 22:29:24 +0800

    update downloader filename format

--- download/download_jiker.py
+++ download/download_jiker.py
@@ -13,14 +13,15 @@ url_data_video = 'https://learn.jiker.com/course/{0}/{1}/{2}'
 path_video_jiker = '/users/wengjianfeng/movies/jiker'
 headers = {
     'Accept': 'application/json',
-    'Set-Cookie': 'QINGCLOUDELB=dd91b9e2eaabd29b1d0bac5e684e6c7e466d62c936b443a78592d63b6f4b026e|Xrz5y|Xrz5y; path=/; HttpOnly',
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODc1NjUzODAsImlzcyI6Imh0dHBzOlwvXC9wYXNzcG9ydC5qaWtlci5jb20iLCJleHAiOjE1OTAxNTczODAsImF1ZCI6Imh0dHBzOlwvXC9qaWtlci5jb20iLCJzdWIiOjIzNjEsImluZm8iOnsiaWQiOjIzNjEsIm5hbWUiOiJqaWtlcjI1Njg5MDMiLCJjb3VudHJ5X2NvZGUiOiI4NiIsInBob25lX251bWJlciI6IjE1NjU5MDI1NzkwIiwiaW52aXRlX3VzZXJfaWQiOjB9fQ.UfWsrKN2H6pfbyGW8XOvnYgASp5cE3R3aVFRekjE8X8',
+    'Set-Cookie': 'QINGCLOUDELB=dd91b9e2eaabd29b1d0bac5e684e6c7e466d62c936b443a78592d63b6f4b026e|XuYBn|XuYBn; path=/; HttpOnly',
+    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1OTIxMzEwNDUsImlzcyI6Imh0dHBzOlwvXC9wYXNzcG9ydC5qaWtlci5jb20iLCJleHAiOjE1OTQ3MjMwNDUsImF1ZCI6Imh0dHBzOlwvXC9qaWtlci5jb20iLCJzdWIiOjIzNjEsImluZm8iOnsiaWQiOjIzNjEsIm5hbWUiOiJqaWtlcjI1Njg5MDMiLCJjb3VudHJ5X2NvZGUiOiI4NiIsInBob25lX251bWJlciI6IjE1NjU5MDI1NzkwIiwiaW52aXRlX3VzZXJfaWQiOjB9fQ.Y2gFu45HmgHRCBW4uzBRItSSGwl_RI3Fqi5zhdOOvDI',
     'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
 }
 
 
 def request_http(url):
     response = requests.get(url, headers=headers)
+    print('code={0},{1}'.format(response.status_code, response.text))
     return json.loads(response.text)
 
 
@@ -30,7 +31,7 @@ def video_save(id_course, id_lesson, lesson_child):
     if os.path.exists(dir) is False:
         directory = Path(dir)
         Path.mkdir(directory, parents=True)
-    videoName = '{0}_{1}_{2}.mp4'.format(id_lesson, lesson_child['id'], lesson_child['name'])
+    videoName = '{0}_{1}_{2}.mp4'.format(id_lesson, lesson_child['id'], lesson_child['name']).replace(' ', '')
     file = '{0}/{1}'.format(dir, videoName)
     if os.path.exists(file) is True:
         print('{0}文件已存在'.format(videoName))
